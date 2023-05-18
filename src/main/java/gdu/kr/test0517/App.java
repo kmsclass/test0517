21b0dad7ac1f8f481abe07c6adb4adb074babd2a
kms <kms.goodee.co.kr>
2023-05-18 09:02:00 +0900

    20230518-0901

---
+++ src/main/java/gdu/kr/test0517/App.java
@@ -0,0 +1,13 @@
+package gdu.kr.test0517;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/test/ArticleDao.java
@@ -0,0 +1,7 @@
+package test;
+
+public interface ArticleDao {
+	void insert(String str)  throws Exception; 
+	void update(String str)  throws Exception; 
+	void delete(String str)  throws Exception; 
+}
